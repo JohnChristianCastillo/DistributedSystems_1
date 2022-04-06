5726e7a0f194ccb9b477325c4f7c4659989c45a6
JohnChristianCastillo <john.castillojandayan@student.uantwerpen.be>
2022-04-06 16:57:08 +0200

    added support for: Submission of routes Clearing of routes Map interactability: -->Correct logic on selecting and deselecting start/end stations -->Only train stations are selectable

--- static/main.js
+++ static/main.js
@@ -7,7 +7,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         '<a href="https://www.goudengids.be/">Gouden Gids</a> contributors'
 }).addTo(map);
 
-
 var dir =
 $(document).ready(function(){
     var $data = $('#data')
@@ -17,10 +16,25 @@ $(document).ready(function(){
         success: function (data){
             $.each(data.station, function(i, station_i){
                 marker = new L.marker([station_i.locationY, station_i.locationX])
-                    .bindPopup(station_i.name)
                     .addTo(map)
+                    .on({
+                        'mouseover': function(e){
+                            L.popup({
+                                offset: [0, -20]
+                            })
+                                .setContent(station_i.name)
+                                .setLatLng(e.latlng)
+                                .openOn(map);
+                            setTimeout(function () {
+                                map.closePopup();
+                            }, 15000)
+                        },
+                        'click': function(){
+                            doRouting(station_i, marker);
+                        }
+                    })
                 //console.log(station_i.name)
-                console.log(station_i.locationX + "---" + station_i.locationY + "===" + station_i.name )
+                //console.log(station_i.locationX + "---" + station_i.locationY + "===" + station_i.name )
             })
             //console.log(data.station)
             //console.log("done printing stations:")
@@ -32,3 +46,70 @@ $(document).ready(function(){
     })
 })
 
+let state = {
+    fromClicked: false,
+    toClicked: false,
+    from: null,
+    to: null,
+    fromName: null,
+    toName: null,
+}
+
+function doRouting(station, marker){
+    if(!state.fromClicked && station.name !== state.toName){
+        state.fromClicked = true;
+        state.from = station;
+        state.fromName = station.name
+        $('#start').val(state.fromName);
+        console.log("From station clicked");
+    }
+    // click second station
+    else if(state.fromClicked && !state.toClicked && station.name !== state.fromName){
+        state.toClicked = true;
+        state.to = station;
+        state.toName = station.name
+        $('#end').val(state.toName);
+        console.log("end station clicked");
+    }
+    // remove from
+    else if(state.fromClicked && station.name === state.fromName){
+        state.fromClicked = false;
+        state.from = null;
+        state.fromName = null;
+        $('#start').val(null);
+        console.log("start station Resetted");
+    }
+    else if(state.toClicked && station.name === state.toName){
+        state.toClicked = false;
+        state.to = null;
+        state.toName = null;
+        $('#end').val(null);
+        console.log("end station Resetted");
+    }
+}
+
+// Clear button click
+document.getElementById("clear").addEventListener("click", function(){
+    for(var member in state) delete state[member];
+    console.log(state);
+    $('#start').val(null);
+    $('#end').val(null);
+})
+
+// Clear button click
+document.getElementById("calculateDirection").addEventListener("click", function(){
+    alert("calculating direction");
+    $.ajax({
+        url: "http://127.0.0.1:5000/route/v1/driving/51.208115,4.421172;51.210481,4.414177?steps=true",
+        type: "GET",
+        success: function (data){
+            console.log(data);
+            //console.log(data.station)
+            //console.log("done printing stations:")
+            //console.log(data);
+        },
+        error: function (error){
+            console.log(error);
+        }
+    })
+})
