f11bfd6b531764e4aed0ac0e7838d700b37c7bd4
JohnChristianCastillo <john.castillojandayan@student.uantwerpen.be>
2022-04-24 17:47:32 +0200

    Added more comments

--- myapi/resources/stations.py
+++ myapi/resources/stations.py
@@ -5,6 +5,10 @@ import pprint
 
 class Stations(Resource):
     def get(self):
+        """
+            Endpoint used to fetch all train stations registered in iRail
+        :return: A json formatted response containing all train stations registered in iRail
+        """
         url = "http://api.irail.be/stations/?format=json&lang=nl"
         payload = {}
         headers = {}
